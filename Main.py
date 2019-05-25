db4cd27d4d7a2324f6806d0dfbe07022fb32bb79
dpovolotskiy <povolotskiyda@yandex.ru>
2019-05-25 22:26:27 +0400

    change output and add argumet --file

--- Main.py
+++ Main.py
@@ -94,6 +94,10 @@ def analyse_graph():
             os.remove(path)
         else:
             break
+
+    main_table = prettytable.PrettyTable()
+    main_table.field_names = ["Graph6", "Number of vertexes", "Number of edges", "Girth",
+                              "Chromatic number"]
     result_girth = {}
     result_chromatic_number = {}
     parser = argparse.ArgumentParser()
@@ -105,21 +109,46 @@ def analyse_graph():
             while str_graph != "":
                 graph = parse_graph6(str_graph)
                 result_girth[str_graph] = girth(graph)
+                result_chromatic_number[str_graph] = chromatic_number(graph)
+                number_of_vertexes = len(graph)
+                number_of_edges = 0
+                for vertex in graph.keys():
+                    number_of_edges += len(graph[vertex])
+                result_to_table = [str_graph, number_of_vertexes, number_of_edges, result_girth[str_graph],
+                                   result_chromatic_number[str_graph]]
+                main_table.add_row(result_to_table)
                 str_graph = input()
         except EOFError:
-            result_str = ""
             count_girths = {}
+            count_chromatic_number = {}
+
             for key, value in result_girth.items():
                 count_girths[value] = count_girths.setdefault(value, 0) + 1
-                result_str += "Girth of graph: {}, equals: {};\n".format(key, value)
-            count_girths = dict(sorted(count_girths.items(), key=lambda girth: girth[1]))
-            main_table = prettytable.PrettyTable()
-            main_table.field_names = ["Girth", "Number of graph with this girth"]
+            count_girths = dict(sorted(count_girths.items(), key=lambda girth: girth[0]))
+
+            for key, value in result_chromatic_number.items():
+                count_chromatic_number[value] = count_chromatic_number.setdefault(value, 0) + 1
+            count_chromatic_number = dict(
+                sorted(count_chromatic_number.items(), key=lambda chromatic_number: chromatic_number[0]))
+
+            girth_stat_table = prettytable.PrettyTable()
+            girth_stat_table.field_names = ["Girth", "Number of graph with this girth"]
             for key, value in count_girths.items():
-                main_table.add_row([key, value])
-            with open("girth_of_graphs.txt", "w") as output:
-                output.write(result_str)
-                output.write(main_table.get_string())
+                girth_stat_table.add_row([key, value])
+
+            chromatic_stat_table = prettytable.PrettyTable()
+            chromatic_stat_table.field_names = ["Chromatic number", "Number of graph with this chromatic number"]
+            for key, value in count_chromatic_number.items():
+                chromatic_stat_table.add_row([key, value])
+
+            with open("main_info.txt", "w") as out_main:
+                out_main.write(main_table.get_string())
+
+            with open("girth_stat.txt", "w") as out_girth_stat:
+                out_girth_stat.write(girth_stat_table.get_string())
+
+            with open("chromatic_number_stat.txt", "w") as out_chromatic_number_stat:
+                out_chromatic_number_stat.write(chromatic_stat_table.get_string())
         finally:
             print("Analyse of graphs was finished!\n")
     else:
@@ -129,9 +158,6 @@ def analyse_graph():
                 graph = parse_graph6(str_graph)
                 result_girth[str_graph] = girth(graph)
                 result_chromatic_number[str_graph] = chromatic_number(graph)
-                count_girths = {}
-                main_table = prettytable.PrettyTable()
-                main_table.field_names = ["Graph6", "Number of vertexes", "Number of edges", "Girth", "Chromatic number"]
                 number_of_vertexes = len(graph)
                 number_of_edges = 0
                 for vertex in graph.keys():
